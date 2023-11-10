77d3d2d85a2e5d1ced2bef36c765acc6f15a6b04
Felipe Johnny <felipijohnny@outlook.com>
2023-11-10 13:03:02 -0300

    aplicando useState no formulario para retornar dados preenchidos

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,9 +1,15 @@
 import './index.css'
 const CampoTexto = (props)=>{
+
+    const aoDigitado = (evento) =>{
+        props.aoAlterado(evento.target.value)
+        console.log(evento.target.value)
+    }
+
     return(
         <div className="campoTexto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder}></input>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}></input>
         </div>
     )
 }
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -2,6 +2,8 @@ import Button from '../Button'
 import CampoTexto from '../CampoTexto'
 import Select from '../Select'
 import './index.css'
+import { useState } from 'react'
+
 const Formulario = () =>{
 
     const times = [
@@ -12,18 +14,41 @@ const Formulario = () =>{
         'Mobile',
     ]
 
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+
+
     const aoSalvar = (event) =>{
         event.preventDefault()
-        console.log("formulario foi subetido")
+        console.log("formulario foi subetido =",nome,cargo,imagem)
     }
 
     return(
         <section className="formulario">
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto obrigatorio={true} label="Nome" placeholder="digite o seu nome"/>
-                <CampoTexto obrigatorio={true} label="Cargo" placeholder="digite o seu cargo"/>
-                <CampoTexto obrigatorio={true} label="Imagem" placeholder="digite o endereço da sua imagem"/>
+                <CampoTexto 
+                    obrigatorio={true}
+                    label="Nome"
+                    placeholder="digite o seu nome"
+                    valor = {nome}
+                    aoAlterado={valor =>setNome(valor)}
+                />
+                <CampoTexto 
+                    obrigatorio={true}
+                    label="Cargo"
+                    placeholder="digite o seu cargo"
+                    valor = {cargo}
+                    aoAlterado={valor =>setCargo(valor)}
+                />
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Imagem"
+                    placeholder="digite o endereço da sua imagem"
+                    valor = {imagem}
+                    aoAlterado={valor =>setImagem(valor)}
+                />
                 <Select obrigatorio={true} label="Setores" itens={times}/>
                 <Button texto="criar card"/>
             </form>
