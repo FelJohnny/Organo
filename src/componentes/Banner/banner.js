a42a324e6693c0d45442d9edebeb38286133a960
Feljohnny <felipijohnny@outlook.com>
2023-11-09 21:01:31 -0300

    iniciando projeto com componentes

---
+++ src/componentes/Banner/banner.js
@@ -0,0 +1,12 @@
+import './banner.css'
+
+function Banner(){
+    return(
+        <header className='banner'>
+            <img src="./imagens/banner.png" alt="Banner principal"/>
+        </header>
+    )
+}
+
+
+export default Banner
